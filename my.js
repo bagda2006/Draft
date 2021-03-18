2819407fa2a0fe4ead1725e9df324f56c083da7f
Bagdasar <bagdasar.aydinyan2006@gmail.com>
2021-03-18 11:53:44 +0300

    first commit

---
+++ my.js
@@ -0,0 +1,19 @@
+//alert('Hello World');//
+// let answer =  confirm('Ты тута?');
+ //console.log(answer);
+ //
+ //let answer = +prompt("У вас было др в этом году", "Да");
+ //console.log(answer);
+ //console.log(typeof(answer));
+ //let incr = 10,
+   //  decr = 10;
+
+//console.log(incr++);
+//console.log(decr--);
+//console.log(9%4);
+//console.log("5" === "5");
+
+// let isChecked = false,
+ //  isClose = false;
+
+//console.log(isChecked || !isClose);
